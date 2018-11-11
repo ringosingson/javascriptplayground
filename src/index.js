a6ab81643d0460f402814f3a9ba914f6cc395125
ringosingson <ringosingson@live.com>
2018-11-11 17:13:11 +0530

    button question
    three solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,53 @@
-//IIFE = immediately invoked function expression
-// - a function that is executed right after it is created
+//Â Solution 1: uses  IIFE
 
-(function doubleNumber(num) {
-  return num * 2;
-})(10);
+function createButtons() {
+  for (var i = 1; i <= 5; i++) {
+    var body = document.getElementsByTagName("BODY")[0];
+    var button = document.createElement("BUTTON");
+    button.innerHTML = "Button " + i;
+    (function(num) {
+      button.onclick = function() {
+        alert("This is button " + num);
+      };
+    })(i);
+    body.appendChild(button);
+  }
+}
+
+createButtons();
+
+// Solution 2:
+
+function createButtons() {
+  for (var i = 1; i <= 5; i++) {
+    var body = document.getElementsByTagName("BODY")[0];
+    var button = document.createElement("BUTTON");
+    button.innerHTML = "Button " + i;
+    addClickFunctionality(button, i);
+    body.appendChild(button);
+  }
+}
+
+function addClickFunctionality(button, num) {
+  button.onclick = function() {
+    alert("This is button " + num);
+  };
+}
+
+createButtons();
+
+// Solution 3:
+
+function createButtons() {
+  for (let i = 1; i <= 5; i++) {
+    var body = document.getElementsByTagName("BODY")[0];
+    var button = document.createElement("BUTTON");
+    button.innerHTML = "Button " + i;
+    button.onclick = function() {
+      alert("This is button " + i);
+    };
+    body.appendChild(button);
+  }
+}
+
+createButtons();
