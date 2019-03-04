a047ceb706bb01d00725c7de73cdcba24cfe3f1a
ringosingson <ringosingson@live.com>
2019-03-04 22:11:27 +0530

    scope and self

---
+++ src/9.7.scope and self.js
@@ -0,0 +1,17 @@
+what is the output
+
+var myCar = {
+    color: "Blue",
+    logColor: function() {
+        var self = this;
+        console.log("In logColor - this.color: " + this.color);
+        console.log("In logColor - self.color: " + self.color);
+        (function() {
+            console.log("In IIFE - this.color: " + this.color);
+            console.log("In IIFE - self.color: " + self.color);
+        })();
+    }
+};
+
+myCar.logColor();
+
