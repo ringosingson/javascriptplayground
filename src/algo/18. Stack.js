2fb87b711e090863227cd4df11332af208f0ac9a
ringosingson <ringosingson@live.com>
2019-09-14 06:52:30 +0530

    stack

---
+++ src/algo/18. Stack.js
@@ -0,0 +1,39 @@
+//what is stack? (somehow same with the call stack)
+// A LIFO(last in first out) data structure eg. stack of plate
+// where stack are used - 1.undo/redo 2. managing function invocation 3. routing(the history object)
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
